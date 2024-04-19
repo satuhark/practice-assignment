3f1059e84213d77b47b90bf765a6b5a9e220f372
satuhark <satu.harkonen2@metropolia.com>
2024-04-19 13:41:10 +0300

    refactoring tests

--- tests/task_api.test.js
+++ tests/task_api.test.js
@@ -1,5 +1,6 @@
 const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
+require('express-async-errors')
 const supertest = require('supertest')
 const mongoose = require('mongoose')
 const helper = require('./test_helper')
@@ -10,14 +11,14 @@ const Task = require('../models/task')
 
 beforeEach(async () => {
     await Task.deleteMany({})
-    let taskObject = new Task(helper.initialTasks[0])
-    await taskObject.save()
-    taskObject = new Task(helper.initialTasks[1])
-    await taskObject.save()
-  })
+    for (let task of helper.initialTasks) {
+        let taskObject = new Task(task)
+        await taskObject.save()
+      }
+    })
 
 test('tasks are returned as json', async () => {
-  await api
+    await api
     .get('/api/tasks')
     .expect(200)
     .expect('Content-Type', /application\/json/)
@@ -27,80 +28,53 @@ test('tasks are returned as json', async () => {
 })
 
 test('tasks are returned as json', async () => {
-    try {
-        const response = await api.get('/api/tasks')
-        assert.strictEqual(response.status, 200)
-        assert(response.headers['content-type'].includes('application/json'))
-        console.log(response.body)
-    } catch (error) {
-        console.error('Error in test:', error)
-        assert.fail(error)
-    }
+    const response = await api.get('/api/tasks')
+    assert.strictEqual(response.status, 200)
+    assert(response.headers['content-type'].includes('application/json'))
+    console.log(response.body)
 })
 
-
 test('a specific task is within the returned tasks', async () => {
-    try {
-        const response = await api.get('/api/tasks')
-        const names = response.body.map(r => r.name)
-        assert(names.includes('Browser can execute only JavaScript'))
-    } catch (error) {
-        console.error('Error in test:', error)
-        assert.fail(error)
-    }
-});
-
-
-  test('a specific task is within the returned tasks', async () => {
     const response = await api.get('/api/tasks')
     const names = response.body.map(r => r.name)
     assert(names.includes('Browser can execute only JavaScript'))
-  })
-  
-  test('a valid task can be added ', async () => {
-    try {
-        const newTask = {
-            name: 'async/await simplifies making async calls',
-            description: "jdsks",
-            deadline: "22.04.2024",
-            status: "To Do"
-        }
-
-        await api
-            .post('/api/tasks')
-            .send(newTask)
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
-        
-        const tasksAtEnd = await helper.tasksInDb()
-        assert.strictEqual(tasksAtEnd.length, helper.initialTasks.length + 1)
-        
-        const names = tasksAtEnd.map(n => n.name)
-        assert(names.includes('async/await simplifies making async calls'))
-    } catch (error) {
-        console.error('Error in test:', error)
-        assert.fail(error)
-    }
 })
 
-    
+test('a specific task is within the returned tasks', async () => {
+    const response = await api.get('/api/tasks')
+    const names = response.body.map(r => r.name)
+    assert(names.includes('Browser can execute only JavaScript'))
+})
+  
+test('a valid task can be added ', async () => {
+    const newTask = {
+        name: 'async/await simplifies making async calls',
+        description: "jdsks",
+        deadline: "22.04.2024",
+        status: "To Do"
+    }
+    await api
+        .post('/api/tasks')
+        .send(newTask)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+        const tasksAtEnd = await helper.tasksInDb()
+        assert.strictEqual(tasksAtEnd.length, helper.initialTasks.length + 1)
+        const names = tasksAtEnd.map(n => n.name)
+        assert(names.includes('async/await simplifies making async calls'))
+    })
+  
 test('task without name is not added', async () => {
-    try {
-        const newTask = {
-            description: "jdsks",
-        }
+    const newTask = {
+        description: "jdsks",
+    }
     await api
         .post('/api/tasks')
         .send(newTask)
         .expect(400)
-
-    const tasksAtEnd = await helper.tasksInDb()
+        const tasksAtEnd = await helper.tasksInDb()
         assert.strictEqual(tasksAtEnd.length, helper.initialTasks.length + 1)
-    } catch (error) {
-        console.error('Error in test:', error)
-        assert.fail(error)
-    }
-})
+    })
 
 test('a specific task can be viewed', async () => {
     const tasksAtStart = await helper.tasksInDb()
