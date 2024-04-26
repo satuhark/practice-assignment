a34f64f54fd8bdeca7091fdc7d9e30cb6e96a84b
satuhark <satu.harkonen2@metropolia.com>
2024-04-26 12:33:42 +0300

    commit

--- controllers/login.js
+++ controllers/login.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
-loginRouter.post('/api/login', async (req, res) => {
+loginRouter.post('/', async (req, res) => {
     console.log('Handling login route')
     const { username, password } = req.body
 
