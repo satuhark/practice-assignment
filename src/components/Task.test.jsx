48f87cd045378e636819b985d70b5641d491cad7
satuhark <satu.harkonen2@metropolia.com>
2024-05-15 13:46:02 +0300

    testing

--- src/components/Task.test.jsx
+++ src/components/Task.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from '../App'
+import Login from '../Login'
 
 test('Login button exists', () => {
   render(<App />)
@@ -24,3 +25,29 @@ test('Register button exists', () => {
     screen.getByPlaceholderText('New password')
     screen.getByPlaceholderText('Confirm new password')
   })
+
+  test('logging in', async () => {
+
+    const setUserMock = vi.fn()
+
+    render(<Login setUser={setUserMock} />)
+
+    const usernameInput = screen.getByPlaceholderText('Username')
+    const passwordInput = screen.getByPlaceholderText('Password')
+
+    fireEvent.change(usernameInput, { target: { value: 'testuser' } })
+    fireEvent.change(passwordInput, { target: { value: 'testpassword' } })
+    
+    const loginButton = screen.getByRole('button', { name: 'Login' })
+    fireEvent.click(loginButton)
+
+    
+    
+    expect(setUserMock).toHaveBeenCalledWith({
+        username: 'testuser',
+        password: 'testpassword'
+    })
+    expect(usernameInput).toHaveValue('')
+    expect(passwordInput).toHaveValue('')
+  }
+)
