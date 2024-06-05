aa4fa4de9d59ba20b1bfe5fdd11d340c5195e9a0
satuhark <satu.harkonen2@metropolia.com>
2024-06-05 10:08:25 +0300

    testing

--- utils/config.js
+++ utils/config.js
@@ -5,10 +5,14 @@ process.env.NODE_ENV === 'test'
 ? process.env.TEST_PORT 
 : process.env.PORT
 
+console.log('PORT:', PORT)
+
 const MONGODB_URI = 
 process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
+
+console.log('current MONGODB_URI:', MONGODB_URI)
 
 module.exports = {
   MONGODB_URI,
