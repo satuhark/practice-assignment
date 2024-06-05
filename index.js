543c64f0e32141913c153121f36a3482aeafbcb6
satuhark <satu.harkonen2@metropolia.com>
2024-06-05 12:37:27 +0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
 
