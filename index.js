2ea7e030975d96865e84f29673ad437ec1102e43
satuhark <satu.harkonen2@metropolia.com>
2024-06-05 09:36:35 +0300

    testing

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const logger = require('./utils/logger')
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
