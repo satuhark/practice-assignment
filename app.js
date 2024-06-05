41eb20f44dbc8800cc01f6022181bd1e5960b58e
satuhark <satu.harkonen2@metropolia.com>
2024-06-05 11:12:58 +0300

    commit

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-  console.log('NODE_ENV:', process.env.NODE_ENV);
-  console.log('MONGODB_URI:', process.env.MONGODB_URI);
-  console.log('TEST_MONGODB_URI:', process.env.TEST_MONGODB_URI)
-
 app.use(express.json())
 app.use(cors())
 //app.use(express.static('dist'))
