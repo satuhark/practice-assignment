a4444c6a1f04c6c06f2e92ec01487a6817ea7aa2
satuhark <satu.harkonen2@metropolia.com>
2024-06-05 10:20:04 +0300

    commit

--- utils/config.js
+++ utils/config.js
@@ -1,18 +1,11 @@
 require('dotenv').config()
 
-const PORT = 
-process.env.NODE_ENV === 'test' 
-? process.env.TEST_PORT 
-: process.env.PORT
-
-console.log('PORT:', PORT)
+const PORT = process.env.PORT
 
 const MONGODB_URI = 
 process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
-
-console.log('current MONGODB_URI:', MONGODB_URI)
 
 module.exports = {
   MONGODB_URI,
