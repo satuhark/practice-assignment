f14d7a85c6943b18baeddfe0b5ad3834b6ee2820
satuhark <satu.harkonen2@metropolia.com>
2024-06-05 11:13:05 +0300

    commit

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3001,
+    },
   test: {
     environment: 'jsdom',
     globals: true,
